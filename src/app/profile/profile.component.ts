55a9c955d018b6704eb0846ffba4421a7e48495d
hollan haule <hollanhaule@hollans-MacBook-Pro.local>
2019-05-18 03:51:49 +0100

    cleanup

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -8,14 +8,30 @@ import { ActivatedRoute } from "@angular/router";
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
+  token = '';
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.queryParamMap.subscribe(queryParams => {
-      var token = queryParams.get("access_token")
-      localStorage.setItem('access_token',token);
+      this.token = queryParams.get("access_token")
+      localStorage.setItem('access_token',this.token);
     })
+
+    
+    var url = 'https://api.linkedin.com/v2/me';
+    //var data = {username: 'example'};
+
+    fetch(url, {
+      method: 'GET', // or 'PUT'
+      //body: JSON.stringify(data), // data can be `string` or {object}!
+      headers:{
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.token,
+        'Connection': 'keep-alive'
+      }
+    }).then(res => res.json())
+    .then(response => console.log('Success:', JSON.stringify(response)))
+    .catch(error => console.error('Error:', error));
   }
 
 }
