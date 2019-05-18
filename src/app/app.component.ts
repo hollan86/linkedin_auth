1a213ec883c203322936158f34e2b8a73268a3de
hollan haule <hollanhaule@hollans-MacBook-Pro.local>
2019-05-18 23:12:06 +0100

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as config from './../../config';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,12 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   showLogin = true;
+  
   ngOnInit(): void {
-    
   }
   //title = 'app';
   linkedin(){
-    window.location.replace('https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=86npi10cn6zkzn&redirect_uri=https%3A%2F%2Fhollan-linkedin.herokuapp.com%2Fauth%2Fcallback&scope=r_liteprofile%20r_emailaddress%20w_member_social')
+    window.location.replace('https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=' + config.client_id + '&redirect_uri=https%3A%2F%2Fhollan-linkedin.herokuapp.com%2Fauth%2Fcallback&scope=r_liteprofile%20r_emailaddress%20w_member_social')
     this.showLogin = false;
   }
 }
