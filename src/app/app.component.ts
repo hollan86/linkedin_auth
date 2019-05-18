5b14038f1e4aef8c6ba5a63d65a19153b5989162
hollan haule <hollanhaule@hollans-MacBook-Pro.local>
2019-05-18 21:56:37 +0100

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  showLogin = true;
   ngOnInit(): void {
     
   }
   //title = 'app';
   linkedin(){
     window.location.replace('https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=86npi10cn6zkzn&redirect_uri=https%3A%2F%2Fhollan-linkedin.herokuapp.com%2Fauth%2Fcallback&scope=r_liteprofile%20r_emailaddress%20w_member_social')
+    this.showLogin = false;
   }
 }
